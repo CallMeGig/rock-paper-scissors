4f5f2029629c1435c61d4765ef575394d7357b11
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-02 00:22:35 +0200

    Add logic for computer's choice.

--- js-rps.js
+++ js-rps.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+// Console rock paper scissors game
+
+function getComputerChoice() {
+    let intChoice = Math.floor(Math.random() * 3);
+
+    if (intChoice == 0) {
+        return "rock";
+    } else if (intChoice == 1) {
+        return "paper";
+    } else return "scissors";
+}
