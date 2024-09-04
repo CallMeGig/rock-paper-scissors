8ad7f5f7b8c4f3942d831b746d6248a91cb43acd
Ghislain <73262104+CallMeGig@users.noreply.github.com>
2024-09-04 13:25:25 +0200

    add logic for a single round in playRound(); add logic to validate user choice in getHumanChoice()

--- js-rps.js
+++ js-rps.js
@@ -1,4 +1,12 @@
-// Console rock paper scissors game
+// Console based rock paper scissors game
+
+// Score variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+// getComputerChoice randomly returns "rock", "paper" or "scissors"
 
 function getComputerChoice() {
     let intChoice = Math.floor(Math.random() * 3);
@@ -10,16 +18,80 @@ function getComputerChoice() {
     } else return "scissors";
 }
 
-function getHumanChoice() {
-    let userChoice = prompt("Pick your weapon", "rock, paper or scissors");
 
-    if (userChoice.toLowerCase() === "rock") {
+// getHumanChoice gets users valid choice and returns it
+
+function getHumanChoice() {
+    const choices = ["rock", "paper", "scissors"]
+    let userChoice = prompt("Pick your weapon.", "rock, paper or scissors").toLowerCase();
+
+    while (!choices.includes(userChoice)) {
+        userChoice = prompt("Please enter one of the aproved weapons.", "Approved weapons: rock, paper or scissors.")
+    }
+
+    if (userChoice === "rock") {
         return "rock";
-    } else if (userChoice.toLowerCase() === "paper") {
+    } else if (userChoice === "paper") {
         return "paper";
-    } else if (userChoice.toLowerCase() === "scissors") {
+    } else if (userChoice === "scissors") {
         return "scissors";
     } else {
 
     }
+}
+
+function playRound(humanChoice, computerChoice) {
+    let roundWon;
+
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "paper") {
+                // computer wins
+                roundWon = false;
+            } else if(computerChoice == "scissors") {
+                // human wins
+                roundWon = true;
+            } else {
+                // tie
+            }
+
+            break;
+        case "paper":
+            if (computerChoice === "scissors") {
+                // computer wins
+                roundWon = false;
+            } else if(computerChoice == "rock") {
+                // human wins
+                roundWon = true;
+            } else {
+                //tie
+            }
+
+            break;
+        case "scissors":
+            if (computerChoice === "rock") {
+                // computer wins
+                roundWon = false;;
+            } else if(computerChoice == "paper") {
+                // human wins
+                roundWon = true;
+            } else {
+                //tie
+            }
+
+            break;
+    }
+
+    switch (roundWon) {
+        case true:
+            console.log(`You win! You played ${humanChoice} which beats ${computerChoice}.`);
+            break;
+        case false:
+            console.log(`You lose! You played ${computerChoice} which loses to ${humanChoice}.`);
+            break;
+        case undefined:
+            console.log(`It's a tie! You both played ${humanChoice}.`);
+            break;
+    }
+
 }
